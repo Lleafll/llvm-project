c8f1ed36277eb01117e8e07a77e9fd252c157ad0
Lars Lorenz <lars.lorenz@zeiss.com>
2023-12-12 23:55:32 +0100

    Refactor test

--- clang-tools-extra/test/clang-tidy/checkers/bugprone/integer-literals-signedness.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/bugprone/integer-literals-signedness.cpp
@@ -3,22 +3,22 @@
 void f(int);
 void g(unsigned);
 
-void assign_unsigned_literal_to_signed() {
+void pass_unsigned_literal_to_signed() {
   f(1U);
   // CHECK-MESSAGES: :[[@LINE-1]]:5: warning: unsigned integer literal assigned to signed integer [bugprone-integer-literals-signedness]
   // CHECK-FIXES: f(1);
 }
 
-void assign_signed_literal_to_unsigned() {
-  g(1);
+void pass_signed_literal_to_unsigned() {
+  g(2);
   // CHECK-MESSAGES: :[[@LINE-1]]:5: warning: signed integer literal assigned to unsigned integer [bugprone-integer-literals-signedness]
   // CHECK-FIXES: g(1U);
 }
 
-void assign_signed_literal_to_signed() {
-  f(123);  // OK
+void pass_signed_literal_to_signed() {
+  f(3);  // OK
 }
 
-void assign_unsigned_literal_to_unsigned() {
-  g(456U);  // OK
+void pass_unsigned_literal_to_unsigned() {
+  g(4U);  // OK
 }
